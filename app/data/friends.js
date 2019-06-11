43afd8381180e69e1c1310e19daa2fe184b5ea23
Connor Perez <connorperez@gmail.com>
2019-06-11 15:17:07 -0500

    basic java and friends list added

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+var friends = [
+
+    {
+        name: "Arya Stark",
+        photo: "https://gayexpress.co.nz/wp-content/uploads/2019/04/Maisie-online.jpg",
+        scores: [
+            "5",
+            "4",
+            "4",
+            "1",
+            "1",
+            "2",
+            "5",
+            "3",
+            "2",
+            "2",
+        ]
+    },
+    {
+        name: "Tyrion Lannister",
+        photo: "https://pbs.twimg.com/profile_images/872582050729119744/kayyK0yt_400x400.jpg",
+        scores: [
+            "5",
+            "4",
+            "2",
+            "2",
+            "1",
+            "1",
+            "5",
+            "4",
+            "5",
+            "1",
+        ]
+    },
+    {
+        name: "Ned Stark",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/8f/Lord_Eddard_Stark_infobox.jpg/revision/latest?cb=20190427041634",
+        scores: [
+            "2",
+            "5",
+            "4",
+            "1",
+            "4",
+            "1",
+            "5",
+            "3",
+            "1",
+            "5",
+        ]
+    },
+    {
+        name: "Sandor Clegane aka The Hound",
+        photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2017/08/pri_50645254.jpg?quality=90&strip=all&zoom=1&resize=644%2C428&ssl=1",
+        scores: [
+            "1",
+            "5",
+            "3",
+            "5",
+            "1",
+            "5",
+            "5",
+            "3",
+            "5",
+            "2",
+        ]
+    },
+    {
+        name: "Jon Snow/Aegon Targaryen",
+        photo: "https://cdn.vox-cdn.com/thumbor/FNrQtFc9fAbrqtz3ftKPxBo5blw=/0x0:3155x2100/1200x800/filters:focal(1326x798:1830x1302)/cdn.vox-cdn.com/uploads/chorus_image/image/56264587/Helen_Sloan___HBO__Photo_4_.0.jpg",
+        scores: [
+            "2",
+            "5",
+            "4",
+            "5",
+            "3",
+            "2",
+            "5",
+            "2",
+            "2",
+            "4",
+        ]
+    },
+    {
+        name: "Daenerys Targaryen",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/daenerys-targaryen-meme-1556127294.png",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "3",
+            "2",
+            "2",
+            "4",
+            "5",
+            "2",
+            "1",
+        ]
+    },
+    {
+        name: "Hot Pie",
+        photo: "https://mondrian.mashable.com/uploads%252Fstory%252Fthumbnail%252F51404%252F2cbfd79b-bd69-490e-98bf-b8ec37a5f3e9.jpg%252F950x534.jpg?signature=QhrrajyOKvRS6AvIwBdIDkHPh6c=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com",
+        scores: [
+            "1",
+            "4",
+            "1",
+            "5",
+            "2",
+            "2",
+            "4",
+            "4",
+            "2",
+            "3",
+        ]
+    },
+    {
+        name: "Sansa Stark",
+        photo: "https://cdn.vox-cdn.com/thumbor/7PqqmuwHBoCh0p_cBtDUQEX4Lbk=/0x0:1234x742/1200x800/filters:focal(514x206:710x402)/cdn.vox-cdn.com/uploads/chorus_image/image/63867627/Screen_Shot_2019_05_20_at_9.46.03_AM.0.png",
+        scores: [
+            "1",
+            "5",
+            "4",
+            "5",
+            "2",
+            "1",
+            "4",
+            "1",
+            "2",
+            "2",
+        ]
+    },
+    {
+        name: "Khal Drogo",
+        photo: "https://cdn3.whatculture.com/images/2014/03/Game-of-Thrones-5.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ]
+    },
+    {
+        name: "Robert Baratheon",
+        photo: "https://i.redd.it/lq1rboy1i4g11.jpg",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "2",
+            "2",
+            "3",
+            "3",
+            "5",
+            "5",
+            "5",
+        ]
+    },
+];
