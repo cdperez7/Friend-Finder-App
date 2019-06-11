efb11181e704d93d457ce98d5d9d42f9a1cb61ea
Connor Perez <connorperez@gmail.com>
2019-06-11 18:02:23 -0500

    api routes finally working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,10 @@
 var friends = require("../data/friends.js");
+var path = require("path");
 
 module.exports = function(app){
 
-    app.get("/api/friends", function(req, res){
-        res.json(friends);
-    })
-};
+    app.get('/api/friends', function (req, res) {
+       return res.json(friends);
+    });
+
+}
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 
-var jsonParser = bodyParser.json();
+// var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+// var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-app.use(bodyParser.json({ type: 'application/**json'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+// app.use(bodyParser.json({ type: 'application/**json' }));
 
-app.use(bodyParser.text({ type: 'text/html'}));
+// app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
+// app.use(bodyParser.text({ type: 'text/html' }));
 
+// require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 
 app.listen(PORT, function(){
